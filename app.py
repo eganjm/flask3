f2eeec2e84c5b1c4c6626eedc70b996531e32431
Mike <j.mike.egan@gmail.com>
2022-09-01 19:38:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template, json, request,redirect,session,jsonify
-from flaskext.mysql import MySQL
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask.ext.mysql import MySQL
+from werkzeug import generate_password_hash, check_password_hash
 
 mysql = MySQL()
 app = Flask(__name__)
 app.secret_key = 'why would I tell you my secret key?'
 
 # MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'root'
-#app.config['MYSQL_DATABASE_PASSWORD'] = 'jay'
-app.config['MYSQL_DATABASE_DB'] = 'BucketList2'
+app.config['MYSQL_DATABASE_USER'] = 'jay'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'jay'
+app.config['MYSQL_DATABASE_DB'] = 'BucketList'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
@@ -45,6 +45,54 @@ def userHome():
 def logout():
     session.pop('user',None)
     return redirect('/')
+
+@app.route('/deleteWish',methods=['POST'])
+def deleteWish():
+    try:
+        if session.get('user'):
+            _id = request.form['id']
+            _user = session.get('user')
+
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.callproc('sp_deleteWish',(_id,_user))
+            result = cursor.fetchall()
+
+            if len(result) is 0:
+                conn.commit()
+                return json.dumps({'status':'OK'})
+            else:
+                return json.dumps({'status':'An Error occured'})
+        else:
+            return render_template('error.html',error = 'Unauthorized Access')
+    except Exception as e:
+        return json.dumps({'status':str(e)})
+    finally:
+        cursor.close()
+        conn.close()
+
+
+@app.route('/getWishById',methods=['POST'])
+def getWishById():
+    try:
+        if session.get('user'):
+            
+            _id = request.form['id']
+            _user = session.get('user')
+    
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.callproc('sp_GetWishById',(_id,_user))
+            result = cursor.fetchall()
+
+            wish = []
+            wish.append({'Id':result[0][0],'Title':result[0][1],'Description':result[0][2]})
+
+            return json.dumps(wish)
+        else:
+            return render_template('error.html', error = 'Unauthorized Access')
+    except Exception as e:
+        return render_template('error.html',error = str(e))
 
 @app.route('/getWish')
 def getWish():
@@ -98,6 +146,34 @@ def addWish():
     finally:
         cursor.close()
         conn.close()
+
+@app.route('/updateWish', methods=['POST'])
+def updateWish():
+    try:
+        if session.get('user'):
+            _user = session.get('user')
+            _title = request.form['title']
+            _description = request.form['description']
+            _wish_id = request.form['id']
+
+            
+
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.callproc('sp_updateWish',(_title,_description,_wish_id,_user))
+            data = cursor.fetchall()
+
+            if len(data) is 0:
+                conn.commit()
+                return json.dumps({'status':'OK'})
+            else:
+                return json.dumps({'status':'ERROR'})
+    except Exception as e:
+        return json.dumps({'status':'Unauthorized access'})
+    finally:
+        cursor.close()
+        conn.close()
+
 
 @app.route('/validateLogin',methods=['POST'])
 def validateLogin():
